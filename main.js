f7e5ad5ab10bedafcba697ed2782820eb35c29fb
えま\k <100063667+emak-gg@users.noreply.github.com>
2023-05-29 21:35:56 +0900

    main.js を更新
    chatgpt有能かよ

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-
+$(window).on('load', function () {
+  var element = $(".typing");
+  var thisChild = ""
+  element.each(function () {
+    var text = $(this).html();
+    var textbox = "";
+    text.split('').forEach(function (target) {
+      if (target !== " ") {
+        textbox += '<span>' + target + '</span>';
+      } else {
+        textbox += target;
+      }
+    });
+    $(this).html(textbox);
+  });
+   $(element).each(function () {
+      thisChild = $(this).children();
+      thisChild.each(function (i) {
+        var time = 100;
+        $(this).delay(time * i).fadeIn(time);
+      });
+  });
+});
