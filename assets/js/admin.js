24be6af3f80eed1a506191e5dafa49eb06002094
kurages <contact@kurages.net>
2023-06-03 19:43:12 +0900

    test

---
+++ assets/js/admin.js
@@ -0,0 +1,16 @@
+
+const admminList = [
+	{
+		"user_id": "kurages",
+		"user_tag": "0255",
+		"pri_color": "#55a4ff",
+		"accent_color": "#9c999a",
+		"member_since": "2022.07.24",
+		"icon": "https://cdn.discordapp.com/avatars/1000436365269422170/47e94a1728cd3544c1be256f2c2baa72.webp?size=80",
+		"banner_color": "#9c999a"
+	},
+]
+
+
+
+
